8c097c65759c6dc4ab4a7e7dc6a4da655254e9d8
fj <castelloiv@gmail.com>
2020-06-05 19:51:51 -0400

    finally have graphql working

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,20 +1,26 @@
 import React from "react"
 
 const Mytest = ({ data }) => {
-  const title = data.markdownRemark.frontmatter.title
+  const title = data.allMarkdownRemark.edges
+  console.log(title)
   return (
     <div>
-      <h3>{title}</h3>
+      <h3>{title[1].node.frontmatter.title}</h3>
+
       <p>hi</p>
     </div>
   )
 }
 
-export const getdata = graphql`
-  query MyQuery {
-    markdownRemark(fileAbsolutePath: { regex: "/sectionone.md/" }) {
-      frontmatter {
-        title
+export const query = graphql`
+  {
+    allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/main/" } }) {
+      edges {
+        node {
+          frontmatter {
+            title
+          }
+        }
       }
     }
   }
