0d310f29e98b982370e6679ad443edb616c063c8
fj <castelloiv@gmail.com>
2020-06-06 16:32:59 -0400

    About, at sections

---
+++ src/pages/alternative.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout2"
+import Styles from "./test.module.scss"
+const Alternative = ({ data }) => {
+  const mydata = data.allMarkdownRemark.edges
+  return (
+    <Layout>
+      <div className={Styles.mcon}>
+        <h1>{data.allMarkdownRemark.edges[0].node.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: mydata[0].node.html }}></div>
+      </div>
+      <div className={Styles.mcon}>
+        <h1>{data.allMarkdownRemark.edges[1].node.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: mydata[1].node.html }}></div>
+      </div>
+      <div className={Styles.mcon}>
+        <h1>{data.allMarkdownRemark.edges[2].node.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: mydata[2].node.html }}></div>
+      </div>
+      <div className={Styles.mcon}>
+        <h1>{data.allMarkdownRemark.edges[3].node.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: mydata[3].node.html }}></div>
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/alternative/" } }
+      sort: { order: ASC, fields: frontmatter___date }
+    ) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Alternative
