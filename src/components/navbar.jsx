fefb1616272921112497996d02e89fae882f11fc
fj <castelloiv@gmail.com>
2020-06-12 16:04:57 -0400

    Fixing nested anchor nav errors

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -64,31 +64,31 @@ export default () => {
                   to="/basics"
                 >
                   Basics
-                  <div className={Styles.drop}>
-                    <Link
-                      activeClassName={Styles.active}
-                      activeClassName={Styles.active}
-                      to="/whatis"
-                    >
-                      What is HER2?
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      Stages
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      History
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      HER2+ (Positive)
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      HER2- (Negative)
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      link6
-                    </Link>
-                  </div>
                 </Link>
+                <div className={Styles.drop}>
+                  <Link
+                    activeClassName={Styles.active}
+                    activeClassName={Styles.active}
+                    to="/whatis"
+                  >
+                    What is HER2?
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    Stages
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    History
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    HER2+ (Positive)
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    HER2- (Negative)
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    link6
+                  </Link>
+                </div>
               </li>
               <li>
                 <Link
@@ -97,36 +97,36 @@ export default () => {
                   to="/traditional"
                 >
                   Traditional
-                  <div className={Styles.drop}>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      Chemotherapy
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      Immunotherapy
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      Surgery
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      Radiation
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      Herceptin (Trastuzumab)
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      Perjeta (Pertuzumab)
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      Kadcyla
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      Nerlynx (Neratinib)
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      Tykerb (Lapatinib)
-                    </Link>
-                  </div>
                 </Link>
+                <div className={Styles.drop}>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    Chemotherapy
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    Immunotherapy
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    Surgery
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    Radiation
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    Herceptin (Trastuzumab)
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    Perjeta (Pertuzumab)
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    Kadcyla
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    Nerlynx (Neratinib)
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    Tykerb (Lapatinib)
+                  </Link>
+                </div>
               </li>
               <li>
                 <Link
@@ -135,27 +135,27 @@ export default () => {
                   to="/alternative"
                 >
                   Alternative
-                  <div className={Styles.drop}>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      Clinical Trials
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      Fake Treatments
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      link3
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      link4
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      link5
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      link6
-                    </Link>
-                  </div>
                 </Link>
+                <div className={Styles.drop}>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    Clinical Trials
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    Fake Treatments
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    link3
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    link4
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    link5
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    link6
+                  </Link>
+                </div>
               </li>
               <li>
                 <Link
@@ -164,27 +164,27 @@ export default () => {
                   to="/blogposts"
                 >
                   Latest{" "}
-                  <div className={Styles.drop}>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      link1
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      link2
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      link3
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      link4
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      link5
-                    </Link>
-                    <Link activeClassName={Styles.active} to="/nope">
-                      link6
-                    </Link>
-                  </div>
                 </Link>
+                <div className={Styles.drop}>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    link1
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    link2
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    link3
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    link4
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    link5
+                  </Link>
+                  <Link activeClassName={Styles.active} to="/nope">
+                    link6
+                  </Link>
+                </div>
               </li>
               <li>
                 <Link activeClassName={Styles.active} to="/about">
