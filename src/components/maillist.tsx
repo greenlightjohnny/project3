b0253cf7fb8bf897cccd245dda082c603768c774
fj <castelloiv@gmail.com>
2020-06-13 17:41:54 -0400

    More mailchimp form work

--- src/components/maillist.tsx
+++ src/components/maillist.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react"
 import * as Styles from "./mail.module.scss"
+import addToMailchimp from "gatsby-plugin-mailchimp"
 
 const EmailListForm: React.FunctionComponent<{}> = () => {
   const [email, setEmail] = useState("")
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    addToMailchimp(email)
+      .then(data => {
+        alert(data.result)
+      })
+      .catch((error: Error) => {
+        // Errors in here are client side
+        // Mailchimp always returns a 200
+      })
   }
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -16,6 +25,7 @@ const EmailListForm: React.FunctionComponent<{}> = () => {
     <form onSubmit={handleSubmit} className={Styles.EmailListForm}>
       <div className={Styles.con}>
         <h1>Subscribe for updates</h1>
+
         <div className={Styles.Wrapper}>
           <input
             placeholder="Email address"
@@ -25,6 +35,7 @@ const EmailListForm: React.FunctionComponent<{}> = () => {
           />
           <button type="submit">Subscribe</button>
         </div>
+        <small>No spam, we promise!</small>
       </div>
     </form>
   )
